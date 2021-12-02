90ce5596139c14974b1243c34b0c2b983c08714a
Héctor Helí <53790913+hector-heli@users.noreply.github.com>
2021-12-02 11:21:11 -0500

    Base de Proyecto Frontend

--- ApiCliente/src/main/java/com/example/ApiCliente/Controladores/ClienteControlador.java
+++ ApiCliente/src/main/java/com/example/ApiCliente/Controladores/ClienteControlador.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @CrossOrigin(origins="*", methods={RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE})
